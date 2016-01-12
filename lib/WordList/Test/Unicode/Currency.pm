2b5b250bf85c8c094bd5cbfd59b64f066dfff21e
perlancar (@pc-home) <perlancar@gmail.com>
2016-01-12 23:29:56 +0700

    Initial commit

---
+++ lib/WordList/Test/Unicode/Currency.pm
@@ -0,0 +1,17 @@
+package WordList::Test::Unicode::Currency;
+
+# DATE
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+1;
+# ABSTRACT: Currency symbols
+
+__DATA__
+IDR Rp
+USD $
+GBP £
+EUR €
+JPY ¥
